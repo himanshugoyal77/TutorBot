969374f2bd8fd1b2110a9b3a0c4f2050eccad5a9
SadhakKumar <sadhak2003@gmail.com>
2024-10-26 05:22:52 +0530

    added multilingual and speach

--- app/home/[slug]/page.jsx
+++ app/home/[slug]/page.jsx
@@ -96,7 +96,6 @@ const PreferancesPage = ({ params }) => {
           </div>
         </div>
       )}
-      
     </div>
   );
 };
--- components/Chatbot.jsx
+++ components/Chatbot.jsx
@@ -5,6 +5,7 @@ import {
   School2Icon,
   SendHorizontalIcon,
   PencilRuler,
+  Volume2,
 } from "lucide-react";
 import { useEffect, useState } from "react";
 import { Pinecone } from "@pinecone-database/pinecone";
@@ -27,6 +28,7 @@ import { HumanMessage, AIMessage } from "@langchain/core/messages";
 import { auth, firestore } from "../util/firebase";
 import { doc, setDoc, getDoc, updateDoc } from "firebase/firestore";
 import DrawBoard from "./DrawBoard";
+import axios from "axios";
 
 export default function ChatComponent({ bookName }) {
   console.log("bookName", bookName);
@@ -37,6 +39,33 @@ export default function ChatComponent({ bookName }) {
   const [pastMessages, setPastMessages] = useState([]);
   const [loading, setLoading] = useState(false);
   const [draw, setDraw] = useState(false);
+  const [translation, setTranslation] = useState(false);
+
+  const [languages] = useState([
+    { code: "hi-IN", name: "Hindi" },
+    { code: "bn-IN", name: "Bengali" },
+    { code: "kn-IN", name: "Kannada" },
+    { code: "ml-IN", name: "Malayalam" },
+    { code: "mr-IN", name: "Marathi" },
+    { code: "od-IN", name: "Odia" },
+    { code: "pa-IN", name: "Punjabi" },
+    { code: "ta-IN", name: "Tamil" },
+    { code: "te-IN", name: "Telugu" },
+    { code: "gu-IN", name: "Gujarati" },
+  ]);
+  const [selectedLanguage, setSelectedLanguage] = useState("en-IN");
+
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false); // State for dropdown visibility
+
+  // Toggle dropdown visibility
+  const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen);
+
+  // Handle language selection
+  const handleLanguageChange = (code) => {
+    setSelectedLanguage(code);
+    setIsDropdownOpen(false); // Close dropdown after selecting a language
+    console.log(`Selected language: ${code}`);
+  };
 
   useEffect(() => {
     // load from firebase firestore
@@ -100,6 +129,34 @@ export default function ChatComponent({ bookName }) {
     }
   };
 
+  const translateResponse = async (input) => {
+    try {
+      const response = await axios.post(
+        "https://api.sarvam.ai/translate",
+        {
+          input: input, // Correctly set the input parameter
+          source_language_code: "en-IN",
+          target_language_code: selectedLanguage,
+          speaker_gender: "Male",
+          mode: "formal",
+          model: "mayura:v1",
+          enable_preprocessing: true,
+        },
+        {
+          headers: {
+            "api-subscription-key": "609361f5-3167-4583-bcc3-f2c0e4a6f7bd",
+          },
+        }
+      );
+
+      console.log("response", response.data); // Log the response data
+      return response.data; // Return the actual data from the response
+    } catch (error) {
+      console.log("error", error);
+      throw error; // Re-throw the error if needed
+    }
+  };
+
   const handleQuery = async (query) => {
     console.log("inputText", inputText);
     try {
@@ -116,12 +173,15 @@ export default function ChatComponent({ bookName }) {
         await pineconeIndex.describeIndexStats()
       ).namespaces;
 
-      const vectorStore = await PineconeStore.fromExistingIndex(embeddings2, {
-        pineconeIndex,
-        namespace: bookName.toString().includes("National_Science_Tectbook")
-          ? "Science"
-          : bookName,
-      });
+      const vectorStore = await PineconeStore.fromExistingIndex(
+        cohereEmbedding,
+        {
+          pineconeIndex,
+          namespace: bookName.toString().includes("National_Science_Tectbook")
+            ? "Science"
+            : bookName,
+        }
+      );
 
       const retriever = vectorStore.asRetriever();
 
@@ -198,10 +258,17 @@ Conversation so far:
         new HumanMessage(query),
         new AIMessage(resultOne),
       ]);
+      let translatedResult;
+      if (selectedLanguage !== "en-IN") {
+        const response = await translateResponse(resultOne);
+        translatedResult = response.translated_text;
+      } else {
+        translatedResult = resultOne;
+      }
 
       setChatState((prev) => {
         const updatedChat = [...prev.chat];
-        updatedChat[updatedChat.length - 1].response = resultOne;
+        updatedChat[updatedChat.length - 1].response = translatedResult;
         return { chat: updatedChat, status: "idle" };
       });
 
@@ -231,6 +298,68 @@ Conversation so far:
     setIsOpen(!isOpen); // Toggle the open state
   };
 
+  const textToSpeech = async (text) => {
+    try {
+      const response = await axios.post(
+        "https://api.sarvam.ai/text-to-speech",
+        {
+          inputs: [text],
+          target_language_code: selectedLanguage,
+          speaker: "maitreyi",
+          pitch: null,
+          pace: null,
+          loudness: null,
+          speech_sample_rate: 8000,
+          enable_preprocessing: true,
+          model: "bulbul:v1",
+        },
+        {
+          headers: {
+            "api-subscription-key": "609361f5-3167-4583-bcc3-f2c0e4a6f7bd",
+          },
+        }
+      );
+
+      // Extract the base64 audio string from the array
+      let base64Audio = response.data.audios[0];
+
+      // Remove any potential data URL prefix
+      if (base64Audio.startsWith("data:audio")) {
+        base64Audio = base64Audio.split(",")[1];
+      }
+
+      const audioBlob = base64ToBlob(base64Audio, "audio/wav");
+      const audioUrl = URL.createObjectURL(audioBlob);
+      const audio = new Audio(audioUrl);
+      audio.play();
+    } catch (error) {
+      console.error("Error playing audio:", error.message || error);
+    }
+  };
+
+  // Helper function to convert base64 to Blob
+  const base64ToBlob = (base64, mimeType) => {
+    try {
+      const byteCharacters = atob(base64);
+      const byteArrays = [];
+
+      for (let i = 0; i < byteCharacters.length; i += 512) {
+        const slice = byteCharacters.slice(i, i + 512);
+        const byteNumbers = new Array(slice.length);
+        for (let j = 0; j < slice.length; j++) {
+          byteNumbers[j] = slice.charCodeAt(j);
+        }
+        const byteArray = new Uint8Array(byteNumbers);
+        byteArrays.push(byteArray);
+      }
+
+      return new Blob(byteArrays, { type: mimeType });
+    } catch (error) {
+      console.error("Error converting base64 to Blob:", error.message || error);
+      throw error;
+    }
+  };
+
   return (
     <div className="flex h-full flex-col rounded-l-2xl w-full">
       {draw ? (
@@ -238,47 +367,114 @@ Conversation so far:
         <MemoizedIframe url={url} />
       ) : (
         <>
-          <div
-            id="chat-section"
-            className="no-scrollbar h-full flex-grow overflow-y-auto rounded-xl py-4 transition-all duration-300 ease-in-out px-8 p-4"
-          >
-            {chatState.chat.length === 0 && chatState.status !== "loading" ? (
-              <div className="flex h-min flex-col items-center justify-center rounded-xl bg-gray-200 tcenter shadow-md">
-                <School2Icon size={48} className="tgray-600" />
-                <h2 className="mt-4 h-2 tlg font-semibold tgray-700">
-                  How can I assist you today?
-                </h2>
-              </div>
-            ) : (
-              <div className="space-y-4">
-                {chatState.chat.map((message, index) => (
-                  <div key={index} className="h-min">
-                    {/* User Message */}
-                    {message.input !== "" && (
-                      <div className="flex justify-end">
-                        <div className="max-w-xs h-min rounded-lg bg-[#6f42a9] p-3 text-white shadow-md">
-                          {message.input}
+          <div className="relative flex h-full flex-col rounded-l-2xl w-full">
+            <div
+              className="h-min bg-slate-800 ml-auto rounded-t-2xl"
+              style={{ position: "relative", display: "inline-block" }}
+            >
+              <button
+                onClick={toggleDropdown}
+                style={{
+                  padding: "8px 12px",
+                  backgroundColor: "#f1f1f1",
+                  border: "1px solid #ccc",
+                  borderRadius: "4px",
+                  cursor: "pointer",
+                }}
+              >
+                {languages.find((lang) => lang.code === selectedLanguage)
+                  ?.name || "Select Language"}
+              </button>
+
+              {isDropdownOpen && (
+                <ul
+                  style={{
+                    position: "absolute",
+                    top: "100%",
+                    right: "0",
+                    marginTop: "4px",
+                    padding: "8px 0",
+                    width: "200px",
+                    backgroundColor: "#fff",
+                    border: "1px solid #ccc",
+                    borderRadius: "4px",
+                    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+                    listStyle: "none",
+                    zIndex: 10,
+                  }}
+                >
+                  {languages.map((language) => (
+                    <li key={language.code}>
+                      <a
+                        onClick={() => handleLanguageChange(language.code)}
+                        style={{
+                          display: "block",
+                          padding: "8px 12px",
+                          color: "#333",
+                          cursor: "pointer",
+                          textDecoration: "none",
+                        }}
+                        onMouseOver={(e) =>
+                          (e.currentTarget.style.backgroundColor = "#f1f1f1")
+                        }
+                        onMouseOut={(e) =>
+                          (e.currentTarget.style.backgroundColor = "#fff")
+                        }
+                      >
+                        {language.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
+            <div
+              id="chat-section"
+              className="no-scrollbar h-full flex-grow overflow-y-auto rounded-xl py-4 transition-all duration-300 ease-in-out px-8 p-4"
+            >
+              {chatState.chat.length === 0 && chatState.status !== "loading" ? (
+                <div className="flex h-min flex-col items-center justify-center rounded-xl bg-gray-200 tcenter shadow-md">
+                  <School2Icon size={48} className="tgray-600" />
+                  <h2 className="mt-4 h-2 tlg font-semibold tgray-700">
+                    How can I assist you today?
+                  </h2>
+                </div>
+              ) : (
+                <div className="space-y-4">
+                  {chatState.chat.map((message, index) => (
+                    <div key={index} className="h-min">
+                      {/* User Message */}
+                      {message.input !== "" && (
+                        <div className="flex justify-end">
+                          <div className="max-w-xs h-min rounded-lg bg-[#6f42a9] p-3 text-white shadow-md">
+                            {message.input}
+                          </div>
+                        </div>
+                      )}
+                      {/* Chatbot Response */}
+                      <div className="mt-2 flex justify-start">
+                        <div className="relative h-min max-w-xs rounded-lg bg-gray-100 p-3 tgray-900 shadow-md">
+                          {chatState.status === "loading" &&
+                          loadingIndex === index ? (
+                            <div className="flex items-center text-black bg-inherit">
+                              <Ellipsis className="icon animate-pulse" />
+                            </div>
+                          ) : (
+                            message.response
+                          )}
+                          <button
+                            onClick={() => textToSpeech(message.response)}
+                          >
+                            <Volume2 />
+                          </button>
                         </div>
                       </div>
-                    )}
-                    {/* Chatbot Response */}
-                    <div className="mt-2  flex justify-start">
-                      <div className="relative h-min max-w-xs rounded-lg bg-gray-100 p-3 tgray-900 shadow-md">
-                        {chatState.status === "loading" &&
-                        loadingIndex === index ? (
-                          <div className="flex items-center text-black bg-inherit">
-                            <Ellipsis className="icon animate-pulse" />
-                          </div>
-                        ) : (
-                          message.response
-                        )}
-                      </div>
                     </div>
-                  </div>
-                ))}
-                {error && <p style={{ color: "red" }}>{error}</p>}
-              </div>
-            )}
+                  ))}
+                  {error && <p style={{ color: "red" }}>{error}</p>}
+                </div>
+              )}
+            </div>
           </div>
         </>
       )}
